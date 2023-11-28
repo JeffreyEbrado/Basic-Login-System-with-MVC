b4696d5dd9f6a20c02b96277ca500fd0b31eee4a
Jeffrey Ebrado <145235529+JeffreyEbrado@users.noreply.github.com>
2023-11-28 14:26:03 +0800

    Main.java
    adding the database where the file create is organize in one single folder

--- Basic Login System(Completed)/Main.java
+++ Basic Login System(Completed)/Main.java
@@ -6,10 +6,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
-/* Login Part */
         LoginModel loginModel = new LoginModel();
         LoginView loginView = new LoginView();
         LoginController loginController = new LoginController(loginView, loginModel);
+
+        //used to change panel using registerButton from login to register and so on
+        RegisterModel registerModel = new RegisterModel();
+        RegisterView registerView = new RegisterView();
+        RegisterController registerController = new RegisterController(registerView, registerModel);
+
+        UserView userView = new UserView();
+        UserModel userModel = new UserModel();
+        UserController userController = new UserController(userView, userModel);
 
         // Creating the main JFrame to display the panels
         JFrame frame = new JFrame("Login/Register");
@@ -19,44 +27,10 @@ public class Main {
 
         // Adding the login panel initially
         frame.add(loginView.getPanel());
+        frame.setVisible(true);
 
-/* Register Part */   
-        //used to change panel using registerButton from login to register and so on
-        RegisterModel registerModel = new RegisterModel();
-        RegisterView registerView = new RegisterView();
-        RegisterController registerController = new RegisterController(registerView, registerModel);
-         
-        // LOGIN: registerButton -> REGISTER
-        // Switching to register panel when press
-        loginView.getSwitchToRegisterButton().addActionListener(e -> {
-            frame.getContentPane().removeAll();
-            frame.add(registerView.getPanel());
-            frame.revalidate();
-            frame.repaint();
-        });
+/* Login Part */
 
-        //REGISTER: registerButton -> LOGIN
-        // Switching back to login panel when registered
-        registerView.getRegisteredButton().addActionListener(e -> {
-            frame.getContentPane().removeAll();
-            frame.add(loginView.getPanel());
-            frame.revalidate();
-            frame.repaint();
-        });
-        
-        //REGISTER: cancelButton -> LOGIN
-        // Switching back to login panel if cancelled
-        registerView.getCancelButton().addActionListener(e -> {
-            frame.getContentPane().removeAll();
-            frame.add(loginView.getPanel());
-            frame.revalidate();
-            frame.repaint();
-        });
-/* User Part */
-        UserView userView = new UserView();
-        UserModel userModel = new UserModel();
-        UserController userController = new UserController(userView, userModel);
-        
         //LOGIN: loginButton -> USER
         // Assuming successful login action performs the switch to the user panel
         loginView.getLoginButton().addActionListener(e -> {
@@ -65,7 +39,9 @@ public class Main {
             loginModel.setPassword(String.valueOf(loginView.getPasswordField().getPassword()));
 
                 // Check if file exists for the entered username
-                String fileName = loginModel.getUsername() + ".txt";
+                String folderPath = "database";
+                String fileName = folderPath + File.separator + loginModel.getUsername() + ".txt";
+   
                 File file = new File(fileName);
 
                 if (file.exists()) {
@@ -86,14 +62,43 @@ public class Main {
                 }
         });
 
-        // Switching back to login panel from user panel
-        userView.getLogoutButton().addActionListener(e -> {
+        // LOGIN: registerButton -> REGISTER
+        // Switching to register panel when press
+        loginView.getSwitchToRegisterButton().addActionListener(e -> {
+            frame.getContentPane().removeAll();
+            frame.add(registerView.getPanel());
+            frame.revalidate();
+            frame.repaint();
+        });
+
+/* Register Part */   
+        
+        //REGISTER: registerButton -> LOGIN
+        // Switching back to login panel when registered
+        registerView.getRegisteredButton().addActionListener(e -> {
+            frame.getContentPane().removeAll();
+            frame.add(loginView.getPanel());
+            frame.revalidate();
+            frame.repaint();
+        });
+        
+        //REGISTER: cancelButton -> LOGIN
+        // Switching back to login panel if cancelled
+        registerView.getCancelButton().addActionListener(e -> {
             frame.getContentPane().removeAll();
             frame.add(loginView.getPanel());
             frame.revalidate();
             frame.repaint();
         });
 
-        frame.setVisible(true);
+/* User Part */
+
+        // Switching back to login panel from user panel
+        userView.getLogoutButton().addActionListener(e -> {
+            frame.getContentPane().removeAll();
+            frame.add(loginView.getPanel());
+            frame.revalidate();
+            frame.repaint();
+        });   
     }
 }
